ade7bd16d84e327fa7831c34424415e111e38df9
jordigd20 <jordigd8@gmail.com>
2023-11-19 19:07:24 +0100

    feat: add unit tests for webhook controller and service

--- src/webhook/webhook.controller.spec.ts
+++ src/webhook/webhook.controller.spec.ts
@@ -1,14 +1,77 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WebhookController } from './webhook.controller';
 import { WebhookService } from './webhook.service';
+import Stripe from 'stripe';
+import { BadRequestException } from '@nestjs/common';
 
 describe('WebhookController', () => {
   let controller: WebhookController;
 
+  const stripe = new Stripe(process.env.STRIPE_API_KEY, {
+    apiVersion: '2023-08-16'
+  });
+
+  const webhookServiceMock = {
+    handleWebhooks: jest.fn().mockImplementation((signature: string, rawBody: any) => {
+      const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
+      let event;
+
+      try {
+        event = stripe.webhooks.constructEvent(rawBody, signature, endpointSecret);
+      } catch (error) {
+        throw new BadRequestException(`Webhook error: ${error.message}`);
+      }
+
+      switch (event.type) {
+        case 'payment_intent.succeeded':
+          return {
+            id: new Date().getTime(),
+            status: 'COMPLETED',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            receiptUrl: event.data.object.charges.data[0].receipt_url,
+            userId: event.data.object.metadata.userId,
+            addressId: event.data.object.metadata.addressId
+          };
+        default:
+          console.log(`Unhandled event type ${event.type}`);
+          throw new BadRequestException(`Unhandled event type ${event.type}`);
+      }
+    })
+  };
+
+  const paymentIntentSucceededEvent = {
+    id: 'evt_3ODQyrIs1Vk34TSF07xZLune',
+    type: 'payment_intent.succeeded',
+    data: {
+      object: {
+        charges: {
+          object: 'list',
+          data: [
+            {
+              id: 'ch_3OEAHsIs1Vk34TSF11JpLPSR',
+              object: 'charge',
+              receipt_url: 'https://pay.stripe.com/receipts/...'
+            }
+          ]
+        },
+        metadata: {
+          userId: 1,
+          addressId: 2
+        }
+      }
+    }
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [WebhookController],
-      providers: [WebhookService],
+      providers: [
+        {
+          provide: WebhookService,
+          useValue: webhookServiceMock
+        }
+      ]
     }).compile();
 
     controller = module.get<WebhookController>(WebhookController);
@@ -16,5 +79,50 @@ describe('WebhookController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should handle a stripe webhook', () => {
+    const rawBody = JSON.stringify(paymentIntentSucceededEvent, null, 2);
+
+    const header = stripe.webhooks.generateTestHeaderString({
+      payload: rawBody,
+      secret: process.env.STRIPE_WEBHOOK_SECRET
+    });
+
+    expect(controller.webhook(header, { rawBody } as any)).toEqual({
+      id: expect.any(Number),
+      status: 'COMPLETED',
+      receiptUrl: 'https://pay.stripe.com/receipts/...',
+      createdAt: expect.any(Date),
+      updatedAt: expect.any(Date),
+      userId: 1,
+      addressId: 2
+    });
+    expect(webhookServiceMock.handleWebhooks).toHaveBeenCalledWith(header, rawBody);
+  });
+
+  it('should throw an error if stripe-signature header is missing or is not valid', () => {
+    const rawBody = JSON.stringify(paymentIntentSucceededEvent, null, 2);
+    const header = undefined;
+
+    expect(() => controller.webhook(header, { rawBody } as any)).toThrowError(BadRequestException);
+    expect(webhookServiceMock.handleWebhooks).toHaveBeenCalledWith(header, rawBody);
+  });
+
+  it('should throw an error if the event is unhandled', () => {
+    const body = {
+      id: 'evt_3ODQyrIs1Vk34TSF07xZLune',
+      type: 'checkout.session.completed'
+    };
+
+    const rawBody = JSON.stringify(body, null, 2);
+
+    const header = stripe.webhooks.generateTestHeaderString({
+      payload: rawBody,
+      secret: process.env.STRIPE_WEBHOOK_SECRET
+    });
+
+    expect(() => controller.webhook(header, { rawBody } as any)).toThrowError(BadRequestException);
+    expect(webhookServiceMock.handleWebhooks).toHaveBeenCalledWith(header, rawBody);
   });
 });
--- src/webhook/webhook.service.spec.ts
+++ src/webhook/webhook.service.spec.ts
@@ -1,12 +1,83 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WebhookService } from './webhook.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { StripeService } from '../stripe/stripe.service';
+import { ConfigService } from '@nestjs/config';
+import Stripe from 'stripe';
+import { BadRequestException } from '@nestjs/common';
 
 describe('WebhookService', () => {
   let service: WebhookService;
 
+  const stripe = new Stripe(process.env.STRIPE_API_KEY, {
+    apiVersion: '2023-08-16'
+  });
+
+  const mockStripeService = {
+    stripe: {
+      webhooks: {
+        constructEventAsync: jest
+          .fn()
+          .mockImplementation((rawBody: any, signature: string, endpointSecret: string) => {
+            return stripe.webhooks.constructEventAsync(rawBody, signature, endpointSecret);
+          })
+      }
+    }
+  };
+
+  const mockPrismaService = {
+    order: {
+      create: jest.fn().mockImplementation((data: any) => {
+        return {
+          id: new Date().getTime(),
+          status: 'COMPLETED',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          userId: data.data.userId,
+          addressId: data.data.addressId,
+          receiptUrl: data.data.receiptUrl
+        };
+      })
+    }
+  };
+
+  const paymentIntentSucceededEvent = {
+    id: 'evt_3ODQyrIs1Vk34TSF07xZLune',
+    type: 'payment_intent.succeeded',
+    data: {
+      object: {
+        charges: {
+          object: 'list',
+          data: [
+            {
+              id: 'ch_3OEAHsIs1Vk34TSF11JpLPSR',
+              object: 'charge',
+              receipt_url: 'https://pay.stripe.com/receipts/...'
+            }
+          ]
+        },
+        metadata: {
+          userId: 1,
+          addressId: 2
+        }
+      }
+    }
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WebhookService],
+      providers: [
+        WebhookService,
+        ConfigService,
+        {
+          provide: StripeService,
+          useValue: mockStripeService
+        },
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService
+        }
+      ]
     }).compile();
 
     service = module.get<WebhookService>(WebhookService);
@@ -14,5 +85,94 @@ describe('WebhookService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('handleWebhooks', () => {
+    it('should handle a stripe webhook', () => {
+      const rawBody = JSON.stringify(paymentIntentSucceededEvent, null, 2);
+
+      const header = stripe.webhooks.generateTestHeaderString({
+        payload: rawBody,
+        secret: process.env.STRIPE_WEBHOOK_SECRET
+      });
+
+      expect(() => service.handleWebhooks(header, rawBody)).not.toThrowError(BadRequestException);
+      expect(mockStripeService.stripe.webhooks.constructEventAsync).toHaveBeenCalledWith(
+        rawBody,
+        header,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    });
+
+    it('should throw an error if stripe-signature header is missing or is not valid', async () => {
+      const rawBody = JSON.stringify(paymentIntentSucceededEvent, null, 2);
+      const header = undefined;
+
+      await expect(service.handleWebhooks(header, rawBody)).rejects.toThrowError(
+        BadRequestException
+      );
+      expect(mockStripeService.stripe.webhooks.constructEventAsync).toHaveBeenCalledWith(
+        rawBody,
+        header,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    });
+
+    it('should throw an error if event type is not handled', async () => {
+      const body = {
+        id: 'evt_3ODQyrIs1Vk34TSF07xZLune',
+        type: 'checkout.session.completed'
+      };
+
+      const rawBody = JSON.stringify(body, null, 2);
+
+      const header = stripe.webhooks.generateTestHeaderString({
+        payload: rawBody,
+        secret: process.env.STRIPE_WEBHOOK_SECRET
+      });
+
+      await expect(service.handleWebhooks(header, rawBody)).rejects.toThrowError(
+        BadRequestException
+      );
+      expect(mockStripeService.stripe.webhooks.constructEventAsync).toHaveBeenCalledWith(
+        rawBody,
+        header,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    });
+  });
+
+  describe('paymentIntentSucceeded', () => {
+    it('should create an order', async () => {
+      const rawBody = JSON.stringify(paymentIntentSucceededEvent, null, 2);
+      const header = stripe.webhooks.generateTestHeaderString({
+        payload: rawBody,
+        secret: process.env.STRIPE_WEBHOOK_SECRET
+      });
+
+      const event = await stripe.webhooks.constructEventAsync(
+        rawBody,
+        header,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+
+      expect(await service.paymentIntentSucceeded(event)).toEqual({
+        id: expect.any(Number),
+        status: 'COMPLETED',
+        receiptUrl: 'https://pay.stripe.com/receipts/...',
+        createdAt: expect.any(Date),
+        updatedAt: expect.any(Date),
+        userId: 1,
+        addressId: 2
+      });
+      expect(mockPrismaService.order.create).toHaveBeenCalledWith({
+        data: {
+          addressId: 2,
+          userId: 1,
+          status: 'COMPLETED',
+          receiptUrl: 'https://pay.stripe.com/receipts/...'
+        }
+      });
+    });
   });
 });
